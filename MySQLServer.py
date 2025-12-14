fa82e4ad303f34bd9fdc211f35bd15a9cee42ce2
Charity Njeri <wekesacharity@gmail.com>
2025-12-14 19:51:13 +0300

    Fix exception handling for mysql.connector.Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,7 +1,4 @@
-
-
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -15,7 +12,7 @@ def create_database():
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
 
     finally:
@@ -26,3 +23,4 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
