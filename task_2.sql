fcf942b7ca6ff7ddec24114e26c42fb967c458f1
Charity Njeri <wekesacharity@gmail.com>
2025-12-14 20:16:27 +0300

    Create all tables for alx_book_store

---
+++ task_2.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS AUTHORS (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS BOOKS (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    price DOUBLE,
+    publication_date DATE,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS ORDERS (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+    orderDetail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
